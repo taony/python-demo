79e7f3b68ef220e41f4810076be1d2bcf7c48c87
taony <taonyzhang@gmail.com>
2017-03-03 23:49:59 +0800

    Hello

--- 01-HelloPython/HelloPython.py
+++ 01-HelloPython/HelloPython.py
@@ -1,1 +1,38 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+
 print('Hello,Python!');
+
+# 多行注释
+'''
+单引号多行注释
+单引号多行注释
+单引号多行注释
+'''
+
+"""
+双引号多行注释
+双引号多行注释
+双引号多行注释
+"""
+
+# 赋值语句
+
+a=b=c=1;
+
+print a+b+c;
+
+item_one, item_two, item_three = 1, 2, 3;
+
+# 多行语句
+
+total = item_one +\
+        item_two + \
+        item_three;
+
+print(total);
+
+total = ['item_one', 'item_two', 'item_three',
+        'item_four', 'item_five']
+
+print(total);
