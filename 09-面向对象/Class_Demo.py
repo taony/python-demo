60b2a80921498838420c28ae51fe3f7605db4049
taony <taonyzhang@gmail.com>
2017-04-19 23:08:45 +0800

    moudle

--- 09-面向对象/Class_Demo.py
+++ 09-面向对象/Class_Demo.py
@@ -3,6 +3,10 @@
 class Person:
     name = "张三"
     sex = "男"
+    def __init__(self):
+        self.name="zhangsan";
+        self.sex="nv";
+
     def say(self):
         print("我的名字是：",self.name);
 
