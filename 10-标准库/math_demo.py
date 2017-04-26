79176410f8c021f7f6c7c34f774c41cf4a471b75
taony <taonyzhang@gmail.com>
2017-04-26 23:52:52 +0800

    标准库

---
+++ 10-标准库/math_demo.py
@@ -0,0 +1,7 @@
+# -*-encoding:utf-8-*-
+
+import math
+
+print(math.log(1024,2));
+
+print(math.cos(3));
---
+++ 10-标准库/random_demo.py
@@ -0,0 +1,7 @@
+# -*-encoding:utf-8-*-
+
+import random
+
+arr=[1,2,3,4,5];
+
+print(random.choice(arr));
