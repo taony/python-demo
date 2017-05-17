8fd1742fb098f73a1ecd854cb303870d51267a0a
taony <taonyzhang@gmail.com>
2017-05-17 23:15:39 +0800

    标准库

--- 11-多线程/thread_demo.py
+++ 11-多线程/thread_demo.py
@@ -4,6 +4,7 @@ import _thread
 import time
 
 # 为线程定义一个函数
+
 def print_time( threadName, delay):
    count = 0
    while count < 5:
@@ -12,6 +13,7 @@ def print_time( threadName, delay):
       print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
 
 # 创建两个线程
+
 try:
    _thread.start_new_thread( print_time, ("Thread-1", 2, ) )
    _thread.start_new_thread( print_time, ("Thread-2", 4, ) )
